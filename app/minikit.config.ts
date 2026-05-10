85333bf68ef7893579f6e06be6c85ec64f5da42a
Biosolver <asperant001@gmail.com>
2026-05-10 19:00:02 +0700

    Delete app/minikit.config.ts

--- app/minikit.config.ts
+++
@@ -1,6 +0,0 @@
-import { MiniKitConfig } from "@coinbase/minikit";
-const config: MiniKitConfig = {
-  appName: "Base Scout",
-  appUrl: process.env.NEXT_PUBLIC_APP_URL || "https://base-scout.vercel.app",
-};
-export default config;
